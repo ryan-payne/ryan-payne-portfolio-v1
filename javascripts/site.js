fc3600f77435b2bff3767ece443bb774f86e8f3a
Ryan Payne <ryanpayne.me@gmail.com>
2020-10-07 12:35:20 -0400

    Updates to basecamp page and styles

--- javascripts/site.js
+++ javascripts/site.js
@@ -1,1 +1,15 @@
-// This is where it all goes :)
+document.addEventListener(
+  "scroll",
+  function () {
+    var scrollTop =
+      document.documentElement["scrollTop"] || document.body["scrollTop"];
+    var scrollBottom =
+      (document.documentElement["scrollHeight"] ||
+        document.body["scrollHeight"]) - document.documentElement.clientHeight;
+    scrollPercent = scrollTop / scrollBottom * 100 + "%";
+    document
+      .getElementById("progress")
+      .style.setProperty("--scroll", scrollPercent);
+  },
+  { passive: true }
+);
